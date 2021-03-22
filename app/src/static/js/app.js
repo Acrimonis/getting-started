29e7338686e60e757593114705d3531be1771024
nICo BaD <acrimonis@gmail.com>
2021-03-22 08:34:32 +0100

    Get cracking

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Nothing to do?</p>
+                <p className="text-center">Nothing to do ? GEt cracking!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
